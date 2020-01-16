5d2ef10c2044ff94372cef6d267f99037eb3777a
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-01-16 13:00:47 +0100

    Constructores de clases

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -58,9 +58,58 @@ public class Rectangulo {
 
     public Rectangulo(int ladoMenor, int ladoMayor){ //Constructor con parámetros
 
-
+        this.ladoMenor = ladoMenor;
+        this.ladoMayor = ladoMayor;
 
     }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Como es un objeto distinto, tengo que obtener las propiedades del objeto de la clase
+    //Tengo que hacerlo a través del método
+
+    public Rectangulo(Rectangulo nombreDelObjeto){
+
+        this.ladoMenor = nombreDelObjeto.getLadoMenor();
+        this.ladoMayor = nombreDelObjeto.getLadoMayor();
+
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Una vez que tengo los constuctores tengo que crear las funciones
+    //this = Para referirse a las propiedades mismas de la clase
+
+    public int getLadoMenor(){
+
+        return this.ladoMenor;
+
+    }
+
+    public int getLadoMayor(){
+        
+        return this.ladoMayor;
+
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Modificable
+    //Como es un procedimiento, int ladoMenor es el valor por parámetros que le pasamos desde el main
+    //para cambiar el valor de this.ladoMenor
+
+    public void setLadoMenor(int ladoMenor){
+
+        this.ladoMenor = ladoMenor;
+
+    }
+
+    public void setLadoMayor(int ladoMayor){
+
+        this.ladoMayor = ladoMayor;
+
+    }
+
 
     /*
     PROPIEDADES DERIVADAS
