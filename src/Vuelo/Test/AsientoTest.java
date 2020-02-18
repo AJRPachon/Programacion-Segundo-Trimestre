8c1e6544ee156af2ebb6eb6e153902c22f7bbc67
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-02-18 14:39:04 +0100

    Creacion de test

---
+++ src/Vuelo/Test/AsientoTest.java
@@ -0,0 +1,49 @@
+package Vuelo.Test;
+
+import Vuelo.Asiento;
+import org.junit.Test;
+
+class AsientoTest {
+
+    @Test
+    public void getFumadores() {
+
+        Asiento[] obj = new Asiento[4];
+
+        boolean res;
+
+        assertEquals();
+
+
+    }
+
+    @Test
+    public void setFumadores() {
+    }
+
+    @Test
+    public void getNormales() {
+    }
+
+    @Test
+    public void setNormales() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+
+    @Test
+    public void testClone() {
+    }
+
+
+}
