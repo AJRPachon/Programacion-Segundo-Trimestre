9f00d1a0e42945d990eba5a76d5837bdc18288a0
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-02-20 14:38:20 +0100

    Tres en raya, metodos de la clase casilla y creacion del array con objetos casilla

---
+++ src/TresEnRaya/Casilla.java
@@ -0,0 +1,72 @@
+package TresEnRaya;
+
+/*
+    PROPIEDADES BASICAS:
+        int circulo consultable y modificable
+        int equis consultable y modificable
+
+    PROPIEDADES DERIVADAS:
+        Ninguna
+
+    PROPIEDADES COMPARTIDAS:
+        Ninguna
+
+    METODOS BASICOS:
+        getCirculo()
+        setCirculo()
+
+        getEquis()
+        setEquis()
+
+    METODOS AÑADIDOS:
+       Ninguno
+
+
+ */
+
+public class Casilla {
+
+    private boolean circulo;
+    private boolean equis;
+
+    public Casilla(){  //Constructor
+
+        this.circulo = false; // El circulo será 0
+        this.equis = false;   // La equis será 1
+
+    }
+
+    public Casilla(boolean circulo, boolean equis){ //Constructor con parametros
+
+        this.circulo = circulo;
+        this.equis = equis;
+
+    }
+
+///////////////////// GETTERS Y SETTERS CIRCULO /////////////////////////////////////////////////////////////
+
+    public boolean getCirculo(){
+
+        return this.circulo;
+    }
+
+    public boolean setCirculo(){
+
+        return this.circulo = true;
+    }
+
+///////////////////// GETTERS Y SETTERS EQUIS /////////////////////////////////////////////////////////////
+
+    public boolean getEquis(){
+
+        return this.equis;
+    }
+
+    public boolean setEquis(){
+
+        return this.equis = true;
+    }
+
+//TODO //////////////////////// METODOS OVERRIDE ////////////////////////////////////////////////////////////
+
+}
---
+++ src/TresEnRaya/Juego.java
@@ -0,0 +1,82 @@
+package TresEnRaya;
+
+/*
+    PROPIEDADES BASICAS:
+        Casilla casillas[][]
+
+    PROPIEDADES DERIVADAS:
+        Ninguna
+
+    PROPIEDADES COMPARTIDAS:
+        Ninguna
+
+    METODOS BASICOS:
+        getCasilla()
+        getCasillaCirculo()  //Patron de legacion
+        getCasillaEquis()   //Patron de legacion
+
+    METODOS AÑADIDOS:
+       asignarCasillaCirculo()
+       asignarCasillaEquis()
+
+
+ */
+
+public class Juego {
+
+    private Casilla casillas[][];
+
+    public Juego(){  //Constructor
+
+        this.casillas = new Casilla[3][3]; //En el constructor, creo el array bidimensional de 3x3
+
+        for(int cont = 0; cont < casillas.length; cont++){
+
+            for(int cont2 = 0; cont2 < casillas.length; cont2++) {
+                casillas[cont][cont2] = new Casilla(); //Para ponerle un valor por defecto (Lo he inicializado)
+            }                               //Se pone Casilla() porque es el constructor del objeto
+        }
+
+    }
+
+    public Juego(Casilla[][] casillas){   //Constructor con parametros
+
+        this.casillas = casillas;
+    }
+
+//////////////////// GET CASILLA /////////////////////////////////////////////////////////////////////////////////
+
+    public Casilla[][] getCasilla(){
+
+        return this.casillas;
+    }
+
+//////////////////// GETTERS Y SETTERS CASILLA CIRCULO /////////////////////////////////////////////////////////////////////////////////
+
+    public boolean getCasillaCirculo(int posicion1, int posicion2){
+
+        return this.casillas[posicion1-1][posicion2-1].getCirculo();
+    }
+
+    public void setCasillaCirculo(int posicion1, int posicion2){
+
+        this.casillas[posicion1-1][posicion2-1].setCirculo();
+    }
+
+
+//////////////////// GETTERS Y SETTERS CASILLA EQUIS /////////////////////////////////////////////////////////////////////////////////
+
+    public boolean getCasillaEquis(int posicion1, int posicion2){
+
+        return this.casillas[posicion1-1][posicion2-1].getEquis();
+    }
+
+    public void setCasillaEquis(int posicion1, int posicion2){
+
+        this.casillas[posicion1-1][posicion2-1].setEquis();
+    }
+
+
+//TODO //////////////////////// METODOS OVERRIDE ////////////////////////////////////////////////////////////
+
+}
