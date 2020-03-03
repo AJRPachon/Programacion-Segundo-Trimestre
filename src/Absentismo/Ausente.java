9eb727fbe43fe3382452bfa199c2668704f0a855
AJRPachon <anturrus@hotmail.com>
2020-03-03 14:25:27 +0100

    absentismo

---
+++ src/Absentismo/Ausente.java
@@ -0,0 +1,19 @@
+package Absentismo;
+
+/*
+
+7. El departamento de Contabilidad de la empresa quiere conocer el alcance del absentismo laboral durante una semana.
+ El número de ausentes se guarda en el array AUSENTES en el que cada fila contiene la cantidad diaria semanal de bajas
+ por departamento numerados del 1 al 5. Se pretende procesar la tabla para:
+    a) Imprimir en pantalla el array completo junto al número del departamento al que pertenecen los datos.
+    b) Total de ausentes por departamento a lo largo de la semana.
+    c) Total de ausentes por día de la semana.
+    d) Total de ausentes en la empresa durante la semana.
+
+ */
+
+public class Ausente {
+
+    
+
+}
