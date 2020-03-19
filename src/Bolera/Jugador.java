56a9991a8300d20e42da5da511b25dec34c5e9c3
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-03-19 11:58:44 +0100

    caca

--- src/Bolera/Jugador.java
+++ src/Bolera/Jugador.java
@@ -28,6 +28,48 @@ package Bolera;
 
 public class Jugador {
 
+    private String nombre;
+    private int puntuacion;
 
+
+////////// CONSTRUCTORES //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Constructor sin parametros
+    public Jugador(){
+
+        this.nombre = " ";
+        this.puntuacion = 0;
+
+    }
+
+
+    //Constructor con parametros
+
+    public Jugador(String nombre, int puntuacion){
+
+        this.nombre = nombre;
+        this.puntuacion = puntuacion;
+
+    }
+
+///////// GETTERS Y SETTERS ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public String getNombre(){
+
+        return this.nombre;
+    }
+
+    public int getPuntuacion(){
+
+        return this.puntuacion;
+    }
+
+    public void setPuntuacion(int valor){
+
+        this.puntuacion = valor;
+    }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
