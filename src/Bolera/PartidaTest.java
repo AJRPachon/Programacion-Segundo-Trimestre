2a2cb4b008f581df2d44471a822672760f19ec29
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-04-08 12:31:20 +0200

    acabar los test de JUnit

---
+++ src/Bolera/PartidaTest.java
@@ -0,0 +1,63 @@
+package Bolera;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PartidaTest {
+
+
+
+    @Test
+    public void crearNombreJugadores() {
+    }
+
+    @Test
+    public void crearJugadores() {
+    }
+
+    @Test
+    public void generarPuntuacionJugadores() {
+    }
+
+    @Test
+    public void mostrarPuntuacionPista() {
+    }
+
+    @Test
+    public void maxPuntuacionPista() {
+    }
+
+
+    @Test
+    public void getPartida() {
+    }
+
+    @Test
+    public void getIDPista() {
+    }
+
+    @Test
+    public void getLongitudPista() {
+    }
+
+    @Test
+    public void getLongitudJugadores() {
+    }
+
+    @Test
+    public void getJugadorNombre() {
+    }
+
+    @Test
+    public void getJugadorPuntuacion() {
+    }
+
+    @Test
+    public void setJugadorPuntuacion() {
+    }
+
+    @Test
+    public void getJugadorIDJugador() {
+    }
+}
